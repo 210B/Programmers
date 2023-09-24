2e57aefc92298bd3d56488e665ea352533213ede
210B <leah8735@gmail.com>
2023-09-24 11:32:16 +0900

    Update 피보나치 수.py

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -5,36 +5,3 @@ def solution(n):
         s[i] = (s[i-1]+s[i-2])%1234567
         
     return s[n]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def solution(n):
-    s=[0]*(n+1)
-    s[1]=1
-    for i in range(2,n+1):
-        s[i] = (s[i-1]+s[i-2])%1234567
-    return s[n]
