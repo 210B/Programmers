fc1d54eae39054bc9afd58383f1e551a195ed8e0
210B <leah8735@gmail.com>
2023-09-24 11:31:30 +0900

    [level 2] Title: 피보나치 수, Time: 39.12 ms, Memory: 13.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,40 @@
+def solution(n):
+    s = [0]*(n+1)
+    s[1] = 1
+    for i in range(2,n+1):
+        s[i] = (s[i-1]+s[i-2])%1234567
+        
+    return s[n]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solution(n):
+    s=[0]*(n+1)
+    s[1]=1
+    for i in range(2,n+1):
+        s[i] = (s[i-1]+s[i-2])%1234567
+    return s[n]
