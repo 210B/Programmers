8f2fb1e9afcadf04f815e1f8c72cefa181f5b2dc
210B <leah8735@gmail.com>
2023-09-24 11:24:21 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,28 @@
 def solution(n):
-    b = bin(n).replace('0b', '0')
-    m = b.rfind('01')
-    k = b[m+2:].count('1')
-    b = b[:m] + '10' + '0'*(len(b)-m-2-k) + '1'*k
-    answer = int(b, 2)
-    return answer
+    count_one_n = bin(n).count('1')
+    
+    while True:
+        n += 1
+        count_one_next = bin(n).count('1')
+        
+        if count_one_next == count_one_n:
+            return n
+
+''' 일부 테케 시간초과
+def solution(n):
+    p = str(bin(n)[2:])
+    index = p.find('01')
+    s = list(bin(n)[2:])
+    
+    if index == -1:
+        index = 0
+        s = ['0'] + s
+    
+    s[index] = '1'
+    s[index+1] = '0'
+    num1 = ''.join(s[:index+1])
+    num2 = ''.join(s[index+1:])
+    n = num2.count('1')
+    num3 = (len(num2)-n)*'0'+n*'1'
+    
+    return int('0b'+num1+num3, 2)'''
