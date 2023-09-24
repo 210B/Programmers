36ea992df6d2001cd6a9811545f031f3d36e68bf
210B <leah8735@gmail.com>
2023-09-24 15:25:05 +0900

    Update 카펫.py

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -3,3 +3,13 @@ from math import sqrt
 def solution(brown, yellow):
     a = sqrt(brown*brown/4-4*yellow-2*brown+4)
     return [brown/4+a/2+1,brown/4-a/2+1]
+
+'''
+def solution(brown, yellow):
+    total = brown + yellow
+    for height in range(3, int(total**0.5) + 1):
+        if total % height == 0:
+            width = total // height
+            if (width - 2) * (height - 2) == yellow:
+                return [width, height]
+'''
